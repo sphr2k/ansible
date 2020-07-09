422bac08bf5e802f8e1d3c1efb596785fa84d006
Devan Carpenter <dvn@pep-project.org>
2020-07-10 00:33:21 +0100

    use systemd service for ansible-pull on boot
    also makes ansible-pull script a bit more robust

--- roles/ansible-pull/templates/ansible-pull.sh
+++ roles/ansible-pull/templates/ansible-pull.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
+{% macro run_playbook(url, name, branch=none) %}
+  start=$(date +%s)
+  ansible-pull -U {{ url }} {% if branch is not none %}-C {{ branch}} {% endif %} -d /var/run/ansible/{{ name }}&> /var/log/ansible-{{ name }}.log
+  code=$?
+
+  if [ -d /var/lib/prometheus/node-exporter ]; then
+    prom_file="/var/lib/prometheus/node-exporter/ansible-playbook-{{ name }}.prom"
+    labels="playbook=\"{{ name }}\", playbook_url=\"{{ url }}\", playbook_branch=\"{{ branch }}\""
+    echo "# HELP ansible_playbook_exit_code The exit code of ansible-pull" > ${prom_file}
+    echo "# TYPE ansible_playbook_exit_code gauge" >> ${prom_file}
+    echo "ansible_playbook_exit_code{${labels}} ${code}" >> ${prom_file}
+
+    echo "# HELP ansible_playbook_time The time a playbook took to run, in seconds" >> ${prom_file}
+    echo "# TYPE ansible_playbook_time gauge" >> ${prom_file}
+    echo "ansible_playbook_time{${labels}} $(expr $(date +%s) - ${start})" >> ${prom_file}
+  fi
+{%- endmacro %}
+
 if [ "$1" == "nodisown" ]; then
   # Sometimes the $PATH gets messed up in cron, so lets start by setting the record straight
   PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-  if [[ -d /usr/ansible ]]; then rm -rf /usr/ansible; fi
-  ansible-pull -U {{ ansible_local.pull.repo }} -C {{ ansible_local.pull.branch }} -d /var/run/ansible/basic &> /var/log/ansible.log
-  code=$?
 
-  if [[ "$code" -ne "0" ]]; then
-    # ansible localhost -m irc -a "server=irc.oftc.net use_ssl=yes port=6697 channel=# msg='[$(hostname -f)] Ansible Pull failed.'  nick=ansible-$RANDOM color=red timeout=60"
-    exit 1
-  fi
-
+{{ run_playbook(ansible_local.pull.repo, "basic", ansible_local.pull.branch) }}
 {% if ansible_fqdn in additional_playbooks %}
 {% for playbook in additional_playbooks[ansible_fqdn] %}
-  ansible-pull -U {{ playbooks[playbook.name] }} {% if 'branch' in playbook %}-C {{ playbook.branch}} {% endif %} -d /var/run/ansible/{{playbook.name}}&> /var/log/ansible-{{ playbook.name }}.log
-  code=$?
-
-  if [[ "$code" -ne "0" ]]; then
-    # ansible localhost -m irc -a "server=irc.oftc.net use_ssl=yes port=6697 channel=# msg='[$(hostname -f)] ansible-pull failed with additional playbook {{ playbook.name }}'  nick=ansible-$RANDOM color=red"
-    exit 1
-  fi
-
+{{ run_playbook(playbooks[playbook.name], playbook.name, ansible_local.pull.branch) }}
 {% endfor %}
 {% endif %}
-    # ansible localhost -m irc -a "server=irc.oftc.net use_ssl=yes port=6697 channel=# msg='[$(hostname -f)] Ansible Pull successfully ran'  nick=ansible-$RANDOM color=green"
 
+{% if "additional_playbooks" in ansible_local %}
+{% for playbook in ansible_local.additional_playbooks %}
+{{ run_playbook(playbook.url, playbook.name, playbook.branch) }}
+{% endfor %}
+{% endif %}
 else
   $0 nodisown & disown
 fi
